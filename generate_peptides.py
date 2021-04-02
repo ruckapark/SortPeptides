56b0d6edda0b470422a1a149b6902790ecc5e815
George Ruck <georgeadruck@gmail.com>
2021-04-02 14:54:58 +0200

    Sort peptide classes

---
+++ generate_peptides.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 14:15:42 2021
+
+Trier peptides maxime
+
+@author: George Ruck
+"""
+
+#import relevant libs
+import pandas as pd
+import numpy as np
+
+#read csv file
+def read_csv_peptides(filename):
+    
+    file = pd.read_csv(filename,index_col = 0)
+    return file
+
+def scan_file(df):
+    return None
+
+if __name__ == '__main__':
+    
+    file = read_csv_peptides('peptides_1.csv')
+    results = pd.DataFrame(columns = ['Data','Peptide1','Peptide2','Peptide3'])
+    for x,data in enumerate(file.columns):
+        
+        if file[data].isnull().sum() != file.shape[0]:
+            temp = file[data].dropna().sort_values(ascending = False)
+            
+            if temp.size>2:
+                results.loc[x] = {'Data':data,'Peptide1':temp.index[0],'Peptide2':temp.index[1],'Peptide3':temp.index[2]}
+            elif temp.size > 1:
+                results.loc[x] = {'Data':data,'Peptide1':temp.index[0],'Peptide2':temp.index[1],'Peptide3':np.nan}
+            else:
+                results.loc[x] = {'Data':data,'Peptide1':temp.index[0],'Peptide2':np.nan,'Peptide3':np.nan}
+            
+            
